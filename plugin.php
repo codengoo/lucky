40419804867030b64dd2b26227d21c47f05db146
Tuan Nghia <fakenoname02@gmail.com>
2024-02-28 09:02:24 +0700

    a

---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link            https://wp-vue-kickstart.com
+ * @since           1.0.0
+ * @package         WP_Vue_KickStart
+ *
+ * Plugin Name: WP Vue KickStart
+ * Plugin URI: https://wp-vue-kickstart.com
+ * Description: A wp vue starter for plugin development.
+ * Version: 1.0.0
+ * Author: Md. Rabiul Islam Robi
+ * Author URI: https://robizstory.me
+ * License: GPL v3
+ * Text-Domain: textdomain
+ */
+?>
